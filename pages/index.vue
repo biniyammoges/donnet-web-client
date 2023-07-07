57853d10fbba17d61235be03e6b58ce4ed4e1f07
Biniyam Moges <biniyammoges54@gmail.com>
2023-07-07 23:03:52 +0300

    client intial commit

---
+++ pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1 class="bg-bb-1 text-white">Home</h1>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        bb: {
+          1: "#333",
+        },
+      },
+    },
+  },
+};
